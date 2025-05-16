fc12420bff3e586ddc4bdd83e679c2430f7f5154
KRRISH AGARWAL <krrishagarwal07@gmail.com>
2025-05-16 19:10:36 +0530

    Join Group pop up created

--- ARS/FRONTEND/arsfrontend/src/App.js
+++ ARS/FRONTEND/arsfrontend/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import AssgnCard from "./components/Card.js";
 import Loginform from "./components/Loginform.js";
+import Joingrouppopup from "./components/Joingrouppopup.js";
 
 function App() {
   return (
     <>
       {/* <Sidebar/> */}
       {/* <AssgnCard/> */}
-      <Loginform />
-    </>
+      {/* <Loginform /> */}
+      <Joingrouppopup />
+      </>
   );
 }
 
